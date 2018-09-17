deafe188b4ceec02c184094b9a0f056e922f5678
luffy333221 <43271814+luffy333221@users.noreply.github.com>
2018-09-17 15:33:29 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Youtube from './youtube';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Youtube />, document.getElementById('root'));
-registerServiceWorker();
