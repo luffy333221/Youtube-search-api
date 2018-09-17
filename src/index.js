aec2f6adc9b78be3e1592210bdc0ca83f0d90ec7
luffy333221 <43271814+luffy333221@users.noreply.github.com>
2018-09-17 16:53:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// Import bootstrap css
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Youtube from './youtube';
 
 ReactDOM.render(<Youtube />, document.getElementById("youtube"));
